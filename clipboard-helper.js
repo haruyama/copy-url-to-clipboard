656d8bc6340cbbfd07595e8561086d046f0f9fd4
HARUYAMA Seigo <haruyama@unixuser.org>
2017-12-14 20:36:47 +0900

    Use es6 style for clipboard-helper.js

--- clipboard-helper.js
+++ clipboard-helper.js
@@ -1,8 +1,8 @@
 // This function must be called in a visible page, such as a browserAction popup
 // or a content script. Calling it in a background page has no effect!
 /* global document */
-function copyToClipboard(text, html) {
-  function oncopy(event) {
+const copyToClipboard = (text, html) => {
+  const oncopy = (event) => {
     document.removeEventListener('copy', oncopy, true);
     // Hide the event from the page to prevent tampering.
     event.stopImmediatePropagation();
@@ -11,9 +11,9 @@ function copyToClipboard(text, html) {
     event.preventDefault();
     event.clipboardData.setData('text/plain', text);
     event.clipboardData.setData('text/html', html);
-  }
+  };
   document.addEventListener('copy', oncopy, true);
 
   // Requires the clipboardWrite permission, or a user gesture:
   document.execCommand('copy');
-}
+};
